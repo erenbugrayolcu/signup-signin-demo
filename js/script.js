a3cf73fd98eeba10cc9bf57bb2777739ea76378c
Eren Bugra YOLCU <yolcubugra7@gmail.com>
2021-10-06 17:56:30 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,44 @@
-function signUp(e){
+function showFormUp(){
+    textUp = document.getElementById('sign-up-box');
+    textIn = document.getElementById('sign-in-box');
 
-    sUp = document.getElementById('sign-up-box');
-    sIn = document.getElementById('sign-in-box');
+    textUp.style.visibility = "visible";
+    textIn.style.visibility = "hidden";
+}
 
-    sUp.style.visibility = "visible";
-    sIn.style.visibility = "hidden";
+function showFormIn(){
+    textUp = document.getElementById('sign-up-box');
+    textIn = document.getElementById('sign-in-box');
 
+    textUp.style.visibility = "hidden";
+    textIn.style.visibility = "visible";
+}
+
+var newUser;
+var newPass;
+var uname;
+var pass;
+
+function signUp(){
+
+    newUser = document.getElementById('newUsername').value;
+    newPass = document.getElementById('newPassword').value;
+    
+    window.localStorage.setItem('newUser', newUser);
+    window.localStorage.setItem('newPass', newPass);
 }
 
 function signIn(){
-    sUp = document.getElementById('sign-up-box');
-    sIn = document.getElementById('sign-in-box');
 
-    sUp.style.visibility = "hidden";
-    sIn.style.visibility = "visible";
+    uname = document.getElementById('uname').value;
+    pass = document.getElementById('pass').value;
+
+    newUser = window.localStorage.getItem('newUser');
+    newPass = window.localStorage.getItem('newPass');
+
+    if(uname == newUser && pass == newPass){
+        location.assign("https://https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }else{
+        window.alert("Username or password is wrong, please try again.");
+    }
 }
